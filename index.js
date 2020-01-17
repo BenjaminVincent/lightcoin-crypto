6acf26c3c74cb0b8b3f2d52f9590cb9624c193e6
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-17 22:49:09 +0000

    added withdrawal, transaction, deposite and account classes

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
 let balance = 500.00;
 
-class Withdrawal {
 
-  constructor(amount) {
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let i = 0; i < this.transactions.length; i++) {
+      balance += this.transactions[i].value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
 
 
 
-// DRIVER CODE BELOW
-// We use the code below to "drive" the application logic above and make sure it's working as expected
+class Withdrawal extends Transaction {
 
-t1 = new Withdrawal(50.25);
+  get value() {
+    return this.amount * -1;
+  }
+
+}
+
+class Deposite extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+}
+
+
+
+// DRIVER CODE
+
+myAccount = new Account("Ben");
+
+t1 = new Withdrawal(50.25, myAccount);
+t2 = new Deposite(999.99, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
-
-t2 = new Withdrawal(9.99);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+
+console.log(myAccount.balance);
